231d90c3ec455547398b9cafec270b8ba78e02a5
jhunderdog <33382water@gmail.com>
2021-11-25 14:54:15 +0900

    Update & Delete

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_database/add_todo.dart';
 import 'package:hive_database/todo.dart';
@@ -28,11 +29,41 @@ class _HomeScreenState extends State<HomeScreen> {
                 );
               } else {
                 return ListView.builder(
+                    reverse: true,
+                    shrinkWrap: true,
                     itemCount: box.length,
                     itemBuilder: (context, index) {
                       Todo todo = box.getAt(index);
                       return ListTile(
-                        title: Text(todo.title),
+                        title: Text(
+                          todo.title,
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: todo.isCompleted
+                                  ? Colors.green
+                                  : Colors.black,
+                              decoration: todo.isCompleted
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none),
+                        ),
+                        leading: Checkbox(
+                          value: todo.isCompleted,
+                          onChanged: (value) {
+                            Todo newTodo =
+                                Todo(title: todo.title, isCompleted: value!);
+                            box.putAt(index, newTodo);
+                          },
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            box.deleteAt(index);
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text("Todo Deleted Successfully!"),
+                            ));
+                          },
+                        ),
                       );
                     });
               }
